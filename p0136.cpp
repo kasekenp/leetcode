653c845245e95ceff50ca34f5157d98d9a998c3d
kaseken <kento.kaseda@playground.live>
2020-12-20 20:15:02 +0900

    feat: add solution for p0136

---
+++ p0136.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ret = 0;
+        for (auto num : nums)
+        {
+            ret ^= num;
+        }
+        return ret;
+    }
+};
