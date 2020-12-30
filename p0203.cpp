52c0e8bc1986d88ebb01f18d1f04427a48526e7e
kaseken <kento.kaseda@playground.live>
2020-12-30 12:55:17 +0900

    feat: add solution for p0203

---
+++ p0203.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        ListNode *cur = head, *prev = NULL;
+        while (cur != NULL)
+        {
+            if (cur->val == val)
+            {
+                if (cur == head)
+                {
+                    head = cur->next;
+                }
+                else
+                {
+                    prev->next = cur->next;
+                }
+            }
+            else
+            {
+                prev = cur;
+            }
+            cur = cur->next;
+        }
+        return head;
+    }
+};
